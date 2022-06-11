de801b343d78aea6f09e4521790001fd548f86c8
shreemaahw <105507038+shreemaahw@users.noreply.github.com>
2022-06-11 20:09:43 +0530

    Update about-us.js

--- components/app-body/about-us/about-us.js
+++ components/app-body/about-us/about-us.js
@@ -27,12 +27,12 @@ class AboutUs extends HTMLElement {
         this._gridListEle = this.getElementsByTagName('grid-list')[0];
         this._gridListEle.addEventListener('initialized', () => {
             this._gridListEle.itemList = [
-                { title: 'Current student', imgSrc: './../../../img/aboutus/img-6.png', desc: 'Confidently unlock the next step in your career journey.' },
-                { title: 'Student recruitment', imgSrc: './../../../img/aboutus/img-5.png', desc: 'Inspire future students to study with you.' },
-                { title: 'Student society', imgSrc: './../../../img/aboutus/img-4.png', desc: 'Support your members\' career development.' },
-                { title: 'Careers service', imgSrc: './../../../img/aboutus/img-2.png', desc: 'Push graduate career outcomes to new heights.' },
-                { title: 'Industry association', imgSrc: './../../../img/aboutus/img-3.png', desc: 'Offer more value and boost membership engagement.' },
-                { title: 'Employer', imgSrc: './../../../img/aboutus/img-1.png', desc: 'Attract top talent and make smarter hiring decisions.' },
+                { title: 'Current student', imgSrc: './img/aboutus/img-6.png', desc: 'Confidently unlock the next step in your career journey.' },
+                { title: 'Student recruitment', imgSrc: './img/aboutus/img-5.png', desc: 'Inspire future students to study with you.' },
+                { title: 'Student society', imgSrc: './img/aboutus/img-4.png', desc: 'Support your members\' career development.' },
+                { title: 'Careers service', imgSrc: './img/aboutus/img-2.png', desc: 'Push graduate career outcomes to new heights.' },
+                { title: 'Industry association', imgSrc: './img/aboutus/img-3.png', desc: 'Offer more value and boost membership engagement.' },
+                { title: 'Employer', imgSrc: './img/aboutus/img-1.png', desc: 'Attract top talent and make smarter hiring decisions.' },
             ];
         });
     }
