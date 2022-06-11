9ec31cca1a3c340d2ea576fc361446fbc8ad9fbf
shreemaahw <105507038+shreemaahw@users.noreply.github.com>
2022-06-11 20:27:44 +0530

    Add files via upload

--- components/app-body/our-service/our-service.js
+++ components/app-body/our-service/our-service.js
@@ -12,7 +12,7 @@ class OurService extends HTMLElement {
                 <div class="col py-3">
                     <h4 class="d-flex justify-content-center pt-5">Out services</h4>
                     <p>Helping students to make the best possible career and study decisions.</p>
-                    <img class="img img-fluid w-100" src="./../../../img/ourservice/img-0.png">
+                    <img class="img img-fluid w-100" src="./img/ourservice/img-0.png">
                     <p><b><i>Career Discover</i></b> platform is packed with the latest features to engage and inspire our next generation of future leaders. See the following services.</p>
                 </div>
             </div>
@@ -26,10 +26,10 @@ class OurService extends HTMLElement {
         this._gridListEle = this.getElementsByTagName('grid-list')[0];
         this._gridListEle.addEventListener('initialized', () => {
             this._gridListEle.itemList = [
-                { title: 'Virtual events', imgSrc: './../../../img/ourservice/img-1.png', desc: 'Confidently unlock the next step in your career journey.' },
-                { title: 'Employer experiences', imgSrc: './../../../img/ourservice/img-2.png', desc: 'Inspire future students to study with you.' },
-                { title: 'Project based learning', imgSrc: './../../../img/ourservice/img-3.png', desc: 'Support your members\' career development.' },
-                { title: 'Course tasters', imgSrc: './../../../img/ourservice/img-4.png', desc: 'Push graduate career outcomes to new heights.' },
+                { title: 'Virtual events', imgSrc: './img/ourservice/img-1.png', desc: 'Confidently unlock the next step in your career journey.' },
+                { title: 'Employer experiences', imgSrc: './img/ourservice/img-2.png', desc: 'Inspire future students to study with you.' },
+                { title: 'Project based learning', imgSrc: './img/ourservice/img-3.png', desc: 'Support your members\' career development.' },
+                { title: 'Course tasters', imgSrc: './img/ourservice/img-4.png', desc: 'Push graduate career outcomes to new heights.' },
             ];
         });
     }
--- components/app-body/trending-offer/trending-offer.js
+++ components/app-body/trending-offer/trending-offer.js
@@ -5,7 +5,7 @@ class TrendingOffer extends HTMLElement {
         <div class="container">
             <div class="row py-5">
                 <div class="col py-3 text-light">
-                    <img class="img img-fluid w-100" src="./../../../img/trendingoffer/img-0.png">
+                    <img class="img img-fluid w-100" src="./img/trendingoffer/img-0.png">
                     <h4 class="d-flex justify-content-center pt-5">Super Career Fairs</h4>
                     <p>Super Fairs are a new initiative of <b><i>Career Discover</i></b> to help students, employers and universities connect and interact virtually through a series of ultra relevant events. Super Fairs are <b><i>completely free</i></b> to students, employers and universities.</p>
                     <button class="btn btn-warning">Learn more</button>
